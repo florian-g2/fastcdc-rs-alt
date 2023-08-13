5f630fafeb71b30da8b6f02d57e98b1a1e4d1b08
Florian Gäbler <14858479+florian-g2@users.noreply.github.com>
2023-08-13 22:16:51 +0200

    Update Cargo.toml & lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,64 +1,23 @@
 //
 // Copyright (c) 2023 Nathan Fiedler
+// Copyright (c) 2023 Florian Gäbler
 //
 
-//! This crate implements multiple versions of the FastCDC content defined
-//! chunking algorithm in pure Rust. A critical aspect of the behavior of this
-//! algorithm is that it returns exactly the same results for the same input.
+//! # FastCDC-Alt
 //!
+//! This crate is a fork of the original Rust implementation by Nathan Fiedler. ([nlfiedler/fastcdc-rs](https://github.com/nlfiedler/fastcdc-rs))\
+//! Included here is the enhanced 2020 "FastCDC" content defined chunking algorithm described by Wen Xia, et al.\
+//! \
+//! This fork introduces an adjusted and a bit more complicated alternative API to increase the flexibility and reduce the memory overhead.\
+//! The cut points produced by this fork are identical to the ones produced by the original crate.\
+//! \
+//! The README and all docs are adapted to the adjusted API.\
+//! \
 //! To learn more about content defined chunking and its applications, see
 //! [FastCDC: a Fast and Efficient Content-Defined Chunking Approach for Data
 //! Deduplication](https://www.usenix.org/system/files/conference/atc16/atc16-paper-xia.pdf)
 //! from 2016, as well as the subsequent improvements described in the
-//! [paper](https://ieeexplore.ieee.org/document/9055082) from 2020
-//!
-//! ## Migration from pre-3.0
-//!
-//! If you were using a release of this crate from before the 3.0 release, you
-//! will need to make a small adjustment to continue using the same
-//! implementation as before.
-//!
-//! Before the 3.0 release:
-//!
-//! ```no_run
-//! # use fastcdc::ronomon as fastcdc;
-//! # use std::fs;
-//! # let contents = fs::read("test/fixtures/SekienAkashita.jpg").unwrap();
-//! let chunker = fastcdc::FastCDC::new(&contents, 8192, 16384, 32768);
-//! ```
-//!
-//! After the 3.0 release:
-//!
-//! ```no_run
-//! # use std::fs;
-//! # let contents = fs::read("test/fixtures/SekienAkashita.jpg").unwrap();
-//! let chunker = fastcdc::ronomon::FastCDC::new(&contents, 8192, 16384, 32768);
-//! ```
-//!
-//! The cut points produced will be identical to previous releases as the
-//! `ronomon` implementation was never changed in that manner. Note, however,
-//! that the other implementations _will_ produce different results.
-//!
-//! ## Implementations
-//!
-//! This crate had started as a translation of a variation of FastCDC
-//! implemented in the
-//! [ronomon/deduplication](https://github.com/ronomon/deduplication)
-//! repository, written by Joran Dirk Greef. That variation makes several
-//! changes to the original algorithm, primarily to accomodate JavaScript. The
-//! Rust version of this variation is found in the `ronomon` module in this
-//! crate.
-//!
-//! For a canonical implementation of the algorithm as described in the 2016
-//! paper, see the `v2016` crate.
-//!
-//! For a canonical implementation of the algorithm as described in the 2020
-//! paper, see the `v2020` crate. This implementation produces identical cut
-//! points as the 2016 version, but does so a bit faster.
-//!
-//! If you are using this crate for the first time, the `v2020` implementation
-//! would be the most appropriate. It uses 64-bit hash values and tends to be
-//! faster than both the `ronomon` and `v2016` versions.
+//! [paper](https://ieeexplore.ieee.org/document/9055082) from 2020\
 //!
 //! ## Examples
 //!
@@ -66,25 +25,26 @@
 //!
 //! ```no_run
 //! use std::fs;
-//! use fastcdc::v2020;
+//! use fastcdc_alt::FastCDC;
 //! let contents = fs::read("test/fixtures/SekienAkashita.jpg").unwrap();
-//! let chunker = v2020::FastCDC::new(&contents, 4096, 16384, 65535);
-//! for entry in chunker {
-//!     println!("offset={} size={}", entry.offset, entry.length);
+//! let mut chunker = FastCDC::new(4096, 16384, 65535).unwrap();
+//!
+//! for chunk in chunker.as_iterator(&contents) {
+//!     println!("offset={} size={}", chunk.offset, chunk.cutpoint);
 //! }
 //! ```
 //!
 //! The example above is using normalized chunking level 1 as described in
 //! section 3.5 of the 2020 paper. To use a different level of chunking
-//! normalization, replace `new` with `with_level` as shown below:
+//! normalization, replace `new` with `new_advanced` as shown below:
 //!
 //! ```no_run
 //! use std::fs;
-//! use fastcdc::v2020::{FastCDC, Normalization};
+//! use fastcdc_alt::{FastCDC, Normalization};
 //! let contents = fs::read("test/fixtures/SekienAkashita.jpg").unwrap();
-//! let chunker = FastCDC::with_level(&contents, 8192, 16384, 32768, Normalization::Level3);
-//! for entry in chunker {
-//!     println!("offset={} size={}", entry.offset, entry.length);
+//! let mut chunker = FastCDC::new_advanced(8192, 16384, 32768, Normalization::Level3, None).unwrap();
+//! for chunk in chunker.as_iterator(&contents) {
+//!     println!("offset={} size={}", chunk.offset, chunk.cutpoint);
 //! }
 //! ```
 //!
@@ -115,14 +75,16 @@
 //!
 //! ## Large Data
 //!
-//! If processing very large files, the streaming version of the chunkers in the
-//! `v2016` and `v2020` modules may be a suitable approach. They both allocate a
-//! byte vector equal to the maximum chunk size, draining and resizing the
-//! vector as chunks are found. However, using a crate such as `memmap2` can be
-//! significantly faster than the streaming chunkers. See the examples in the
-//! `examples` directory for how to use the streaming versions as-is, versus the
-//! non-streaming chunkers which read from a memory-mapped file.
+//! If processing very large files, the streaming version of the chunker may be
+//! a suitable approach. It allocate a byte vector equal to the maximum
+//! chunk size, draining and resizing the vector as chunks are found. However,
+//! using a crate such as `memmap2` can be significantly faster than the streaming
+//! chunker. See the examples in the `examples` directory for how to use the
+//! streaming versions as-is, versus the non-streaming chunkers which read from a
+//! memory-mapped file.
+//! Also consider directly leveraging the `cut()` method of the FastCDC struct to
+//! manually implement a streaming functionality.
 
-pub mod ronomon;
-pub mod v2016;
 pub mod v2020;
+
+pub use v2020::*;
