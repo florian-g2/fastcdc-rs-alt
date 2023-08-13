3ac8c8361cd54aeab0e0c24235bc39382973348e
Florian Gäbler <14858479+florian-g2@users.noreply.github.com>
2023-08-13 22:15:29 +0200

    Update async2020 example

--- examples/async2020.rs
+++ examples/async2020.rs
@@ -2,7 +2,7 @@
 // Copyright (c) 2023 Nathan Fiedler
 //
 use clap::{arg, command, value_parser, Arg};
-use fastcdc::v2020::*;
+use fastcdc_alt::*;
 use tokio::fs::File;
 use tokio_stream::StreamExt;
 
@@ -29,13 +29,13 @@ async fn main() {
     let file = File::open(filename).await.expect("cannot open file!");
     let min_size = avg_size / 4;
     let max_size = avg_size * 4;
-    let mut chunker = AsyncStreamCDC::new(file, min_size, avg_size, max_size);
+    let mut chunker = AsyncStreamCDC::new(file, min_size, avg_size, max_size).unwrap();
     let mut stream = Box::pin(chunker.as_stream());
     while let Some(result) = stream.next().await {
-        let entry = result.expect("failed to read chunk");
+        let (_data, chunk) = result.expect("failed to read chunk");
         println!(
             "hash={} offset={} size={}",
-            entry.hash, entry.offset, entry.length
+            chunk.hash, chunk.offset, chunk.cutpoint
         );
     }
 }
