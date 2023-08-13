f6a087e985133ad6df60c85a44dffd66e1939fcf
Florian Gäbler <14858479+florian-g2@users.noreply.github.com>
2023-08-13 22:15:08 +0200

    Adjust async stream implementation

--- src/v2020/async_stream_cdc.rs
+++ src/v2020/async_stream_cdc.rs
@@ -1,5 +1,6 @@
 //
 // Copyright (c) 2023 Nathan Fiedler
+// Copyright (c) 2023 Florian Gäbler
 //
 
 use super::*;
@@ -36,7 +37,7 @@ use async_stream::try_stream;
 ///
 /// ```no_run
 /// # use std::fs::File;
-/// # use fastcdc::v2020::AsyncStreamCDC;
+/// # use fastcdc_alt::v2020::AsyncStreamCDC;
 /// # #[cfg(all(feature = "futures", not(feature = "tokio")))]
 /// # use futures::stream::StreamExt;
 /// # #[cfg(all(feature = "tokio", not(feature = "futures")))]
@@ -44,19 +45,20 @@ use async_stream::try_stream;
 ///
 /// async fn run() {
 ///     let source = std::fs::read("test/fixtures/SekienAkashita.jpg").unwrap();
-///     let mut chunker = AsyncStreamCDC::new(source.as_ref(), 4096, 16384, 65535);
+///     let mut chunker = AsyncStreamCDC::new(source.as_ref(), 4096, 16384, 65535).unwrap();
 ///     let stream = chunker.as_stream();
 ///
 ///     let chunks = stream.collect::<Vec<_>>().await;
 ///
 ///     for result in chunks {
-///         let chunk = result.unwrap();
-///         println!("offset={} length={}", chunk.offset, chunk.length);
+///         let (_data, chunk) = result.unwrap();
+///         println!("offset={} length={}", chunk.offset, chunk.cutpoint);
 ///     }
 /// }
 /// ```
 ///
 pub struct AsyncStreamCDC<R> {
+    inner: FastCDC,
     /// Buffer of data from source for finding cut points.
     buffer: Vec<u8>,
     /// Maximum capacity of the buffer (always `max_size`).
@@ -69,13 +71,6 @@ pub struct AsyncStreamCDC<R> {
     processed: u64,
     /// True when the source produces no more data.
     eof: bool,
-    min_size: usize,
-    avg_size: usize,
-    max_size: usize,
-    mask_s: u64,
-    mask_l: u64,
-    mask_s_ls: u64,
-    mask_l_ls: u64,
 }
 
 impl<R: AsyncRead + Unpin> AsyncStreamCDC<R> {
@@ -84,45 +79,29 @@ impl<R: AsyncRead + Unpin> AsyncStreamCDC<R> {
     ///
     /// Uses chunk size normalization level 1 by default.
     ///
-    pub fn new(source: R, min_size: u32, avg_size: u32, max_size: u32) -> Self {
-        Self::with_level(source, min_size, avg_size, max_size, Normalization::Level1)
+    pub fn new(source: R, min_size: u32, avg_size: u32, max_size: u32) -> Result<Self, Error> {
+        Self::new_advanced(source, min_size, avg_size, max_size, Normalization::Level1)
     }
 
     ///
     /// Create a new `StreamCDC` with the given normalization level.
     ///
-    pub fn with_level(
+    pub fn new_advanced(
         source: R,
         min_size: u32,
         avg_size: u32,
         max_size: u32,
         level: Normalization,
-    ) -> Self {
-        assert!(min_size >= MINIMUM_MIN);
-        assert!(min_size <= MINIMUM_MAX);
-        assert!(avg_size >= AVERAGE_MIN);
-        assert!(avg_size <= AVERAGE_MAX);
-        assert!(max_size >= MAXIMUM_MIN);
-        assert!(max_size <= MAXIMUM_MAX);
-        let bits = logarithm2(avg_size);
-        let normalization = level.bits();
-        let mask_s = MASKS[(bits + normalization) as usize];
-        let mask_l = MASKS[(bits - normalization) as usize];
-        Self {
-            buffer: vec![0_u8; max_size as usize],
+    ) -> Result<Self, Error> {
+        Ok(Self {
+            inner: FastCDC::new_advanced(min_size, avg_size, max_size, level, None)?,
+            buffer: vec![0; max_size as usize],
             capacity: max_size as usize,
             length: 0,
             source,
-            eof: false,
             processed: 0,
-            min_size: min_size as usize,
-            avg_size: avg_size as usize,
-            max_size: max_size as usize,
-            mask_s,
-            mask_l,
-            mask_s_ls: mask_s << 1,
-            mask_l_ls: mask_l << 1,
-        }
+            eof: false,
+        })
     }
 
     /// Fill the buffer with data from the source, returning the number of bytes
@@ -165,43 +144,30 @@ impl<R: AsyncRead + Unpin> AsyncStreamCDC<R> {
 
     /// Find the next chunk in the source. If the end of the source has been
     /// reached, returns `Error::Empty` as the error.
-    async fn read_chunk(&mut self) -> Result<ChunkData, Error> {
+    async fn read_chunk(&mut self) -> Result<(Vec<u8>, Chunk), Error> {
         self.fill_buffer().await?;
         if self.length == 0 {
             Err(Error::Empty)
         } else {
-            let (hash, count) = cut(
-                &self.buffer[..self.length],
-                self.min_size,
-                self.avg_size,
-                self.max_size,
-                self.mask_s,
-                self.mask_l,
-                self.mask_s_ls,
-                self.mask_l_ls,
-            );
-            if count == 0 {
-                Err(Error::Empty)
-            } else {
-                let offset = self.processed;
-                self.processed += count as u64;
-                let data = self.drain_bytes(count)?;
-                Ok(ChunkData {
-                    hash,
-                    offset,
-                    length: count,
-                    data,
-                })
-            }
+            self.inner.set_content_length(self.length);
+
+            let mut chunk = self.inner.cut(&self.buffer[..self.length]).ok_or(Error::Empty)?;
+            // Since the content length is set each invocation, the already set offset is incorrect in this context.
+            chunk.offset = self.processed as usize;
+
+            let data = self.drain_bytes(chunk.cutpoint)?;
+            self.processed += chunk.cutpoint as u64;
+
+            Ok((data, chunk))
         }
     }
 
     #[cfg(all(feature = "tokio", not(feature = "futures")))]
-    pub fn as_stream(&mut self) -> impl Stream<Item = Result<ChunkData, Error>> + '_ {
+    pub fn as_stream(&mut self) -> impl Stream<Item = Result<(Vec<u8>, Chunk), Error>> + '_ {
         try_stream! {
             loop {
                 match self.read_chunk().await {
-                    Ok(chunk) => yield chunk,
+                    Ok(tuple) => yield tuple,
                     Err(Error::Empty) => {
                         break;
                     }
@@ -214,13 +180,13 @@ impl<R: AsyncRead + Unpin> AsyncStreamCDC<R> {
     }
 
     #[cfg(all(feature = "futures", not(feature = "tokio")))]
-    pub fn as_stream(&mut self) -> impl Stream<Item = Result<ChunkData, Error>> + '_ {
+    pub fn as_stream(&mut self) -> impl Stream<Item = Result<(Vec<u8>, Chunk), Error>> + '_ {
         futures::stream::unfold(self, |this| async {
-            let chunk = this.read_chunk().await;
-            if let Err(Error::Empty) = chunk {
+            let tuple = this.read_chunk().await;
+            if let Err(Error::Empty) = tuple {
                 None
             } else {
-                Some((chunk, this))
+                Some((tuple, this))
             }
         })
     }
@@ -233,59 +199,39 @@ mod tests {
     use super::AsyncStreamCDC;
 
     #[test]
-    #[should_panic]
     fn test_minimum_too_low() {
         let array = [0u8; 1024];
-        AsyncStreamCDC::new(array.as_slice(), 63, 256, 1024);
+        assert!(AsyncStreamCDC::new(array.as_slice(), 63, 256, 1024).is_err());
     }
 
     #[test]
-    #[should_panic]
     fn test_minimum_too_high() {
         let array = [0u8; 1024];
-        AsyncStreamCDC::new(array.as_slice(), 67_108_867, 256, 1024);
+        assert!(AsyncStreamCDC::new(array.as_slice(), 67_108_867, 256, 1024).is_err());
     }
 
     #[test]
-    #[should_panic]
     fn test_average_too_low() {
         let array = [0u8; 1024];
-        AsyncStreamCDC::new(array.as_slice(), 64, 255, 1024);
+        assert!(AsyncStreamCDC::new(array.as_slice(), 64, 255, 1024).is_err());
     }
 
     #[test]
-    #[should_panic]
     fn test_average_too_high() {
         let array = [0u8; 1024];
-        AsyncStreamCDC::new(array.as_slice(), 64, 268_435_457, 1024);
+        assert!(AsyncStreamCDC::new(array.as_slice(), 64, 268_435_457, 1024).is_err());
     }
 
     #[test]
-    #[should_panic]
     fn test_maximum_too_low() {
         let array = [0u8; 1024];
-        AsyncStreamCDC::new(array.as_slice(), 64, 256, 1023);
+        assert!(AsyncStreamCDC::new(array.as_slice(), 64, 256, 1023).is_err());
     }
 
     #[test]
-    #[should_panic]
     fn test_maximum_too_high() {
         let array = [0u8; 1024];
-        AsyncStreamCDC::new(array.as_slice(), 64, 256, 1_073_741_825);
-    }
-
-    #[test]
-    fn test_masks() {
-        let source = [0u8; 1024];
-        let chunker = AsyncStreamCDC::new(source.as_slice(), 64, 256, 1024);
-        assert_eq!(chunker.mask_l, MASKS[7]);
-        assert_eq!(chunker.mask_s, MASKS[9]);
-        let chunker = AsyncStreamCDC::new(source.as_slice(), 8192, 16384, 32768);
-        assert_eq!(chunker.mask_l, MASKS[13]);
-        assert_eq!(chunker.mask_s, MASKS[15]);
-        let chunker = AsyncStreamCDC::new(source.as_slice(), 1_048_576, 4_194_304, 16_777_216);
-        assert_eq!(chunker.mask_l, MASKS[21]);
-        assert_eq!(chunker.mask_s, MASKS[23]);
+        assert!(AsyncStreamCDC::new(array.as_slice(), 64, 256, 1_073_741_825).is_err());
     }
 
     struct ExpectedChunk {
@@ -343,7 +289,7 @@ mod tests {
                 digest: "1aa7ad95f274d6ba34a983946ebc5af3".into(),
             },
         ];
-        let mut chunker = AsyncStreamCDC::new(contents.as_ref(), 4096, 16384, 65535);
+        let mut chunker = AsyncStreamCDC::new(contents.as_ref(), 4096, 16384, 65535).unwrap();
         let stream = chunker.as_stream();
 
         let chunks = stream.collect::<Vec<_>>().await;
@@ -351,13 +297,14 @@ mod tests {
         let mut index = 0;
 
         for chunk in chunks {
-            let chunk = chunk.unwrap();
+            let (data, chunk) = chunk.unwrap();
             assert_eq!(chunk.hash, expected_chunks[index].hash);
-            assert_eq!(chunk.offset, expected_chunks[index].offset);
-            assert_eq!(chunk.length, expected_chunks[index].length);
+            assert_eq!(chunk.offset, expected_chunks[index].offset as usize);
+            assert_eq!(chunk.cutpoint, expected_chunks[index].length);
+            assert_eq!(chunk.cutpoint_in_buffer, expected_chunks[index].length);
             let mut hasher = Md5::new();
             hasher
-                .update(&contents[(chunk.offset as usize)..(chunk.offset as usize) + chunk.length]);
+                .update(&contents[chunk.offset..chunk.offset + chunk.cutpoint]);
             let table = hasher.finalize();
             let digest = format!("{:x}", table);
             assert_eq!(digest, expected_chunks[index].digest);
